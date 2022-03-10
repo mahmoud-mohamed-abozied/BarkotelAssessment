3d7639f512ee86e03e4f0cba228eeb060f400d33
Mahmoud Mohamed Abozied <mmabozied22@stud.fci-cu.edu.eg>
2022-03-10 20:19:11 +0200

    Adding CRUD operation for author and book

---
+++ BarkotelAssessment/Entities/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarkotelAssessment.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(250)]
+        public string Title { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime? DateOfPublication { get; set; }
+
+        public byte[] Cover { get; set; }
+
+        public string Description { get; set; }
+        public ICollection<Author> Authors { set; get; } = new List<Author>();
+        public List<AuthorBook> AuthorBook { set; get; } = new List<AuthorBook>();
+
+
+
+    }
+}
